bd885977def7b1a24a66c0f201f669edd4f6cbef
aokitashipro <webmocha2@gmail.com>
2021-04-20 06:32:48 +0900

    属性で設定

---
+++ resources/views/components/tests/card.blade.php
@@ -0,0 +1,5 @@
+<div class="border-2 shadow-md w-1/4 p-2">
+  <div>{{ $title }}</div>
+  <div>画像</div>
+  <div>{{ $content }}</div>
+</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,4 +1,6 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー1</x-slot>
 コンポーネントテスト1
+
+  <x-tests.card title="タイトル1" content="本文1" />
 </x-tests.app>
