886f321e064eaceaa48638b75d949083498dc0a7
aokitashipro <webmocha2@gmail.com>
2021-04-21 05:23:27 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+  'title',
+  'message' => '初期値です。',
+  'content' => '本文初期値です'
+ ])
+
 <div class="border-2 shadow-md w-1/4 p-2">
   <div>{{ $title }}</div>
   <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
 コンポーネントテスト1
 
   <x-tests.card title="タイトル1" content="本文1" :message="$message"  />
+  <x-tests.card title="タイトル2" />
 </x-tests.app>
