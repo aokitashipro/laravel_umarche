2435d4973426c332e802a781464db59667507525
aokitashipro <webmocha2@gmail.com>
2021-04-21 05:53:41 +0900

    属性バッグ

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -4,7 +4,9 @@
   'content' => '本文初期値です'
  ])
 
-<div class="border-2 shadow-md w-1/4 p-2">
+<div {{ $attributes->merge([
+  'class' => 'border-2 shadow-md w-1/4 p-2'
+  ]) }} >
   <div>{{ $title }}</div>
   <div>画像</div>
   <div>{{ $content }}</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,4 +4,5 @@
 
   <x-tests.card title="タイトル1" content="本文1" :message="$message"  />
   <x-tests.card title="タイトル2" />
+  <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 </x-tests.app>
