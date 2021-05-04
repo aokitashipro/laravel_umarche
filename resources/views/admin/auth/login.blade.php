8bb1b7a5d3a94aca3d3c9bb67b4d6192e2c9c122
aokitashipro <webmocha2@gmail.com>
2021-05-05 06:56:11 +0900

    divで調整

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -2,9 +2,11 @@
     <x-auth-card>
         管理者用
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            <div class="w-28">
+                <a href="/">  
+                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                </a>
+            </div>
         </x-slot>
 
         <!-- Session Status -->
