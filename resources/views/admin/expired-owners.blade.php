fc5129fcabfd861cae66cb98fd01a73d09262895
aokitashipro <webmocha2@gmail.com>
2021-05-13 05:35:48 +0900

    btn削除

--- resources/views/admin/expired-owners.blade.php
+++ resources/views/admin/expired-owners.blade.php
@@ -13,9 +13,7 @@
                 <section class="text-gray-600 body-font">
                   <div class="container px-5 mx-auto">
                     <x-flash-message status="session('status')" />
-                    <div class="flex justify-end mb-4"> 
-                      <button onclick="location.href='{{ route('admin.owners.create')}}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>                        
-                    </div>
+                    
                     <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                       <table class="table-auto w-full text-left whitespace-no-wrap">
                         <thead>
