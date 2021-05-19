59323f0b25ee66364103e5f04bfe96c660f826bd
aokitashipro <webmocha2@gmail.com>
2021-05-20 04:35:00 +0900

    index画面

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -32,8 +32,8 @@ class ShopController extends Controller
 
     public function index()
     {
-        $ownerId = Auth::id();
-        $shops = Shop::where('owner_id', $ownerId)->get();
+        //$ownerId = Auth::id();
+        $shops = Shop::where('owner_id', Auth::id())->get();
 
         return view('owner.shops.index', 
         compact('shops'));
--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -10,7 +10,27 @@
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200">
                   @foreach ($shops as $shop )
-                    {{ $shop->name }}
+                    <div class="w-1/2 p-4">
+                    <a href="{{ route('owner.shops.edit', ['shop' => $shop->id ])}}">  
+                    <div class="border rounded-md p-4">
+                      <div class="mb-4">
+                      @if($shop->is_selling)
+                        <span class="border p-2 rounded-md bg-blue-400 text-white">販売中</span>
+                      @else
+                      <span class="border p-2 rounded-md bg-red-400 text-white">停止中</span>
+                      @endif  
+                      </div>
+                      <div class="text-xl">{{ $shop->name }}</div>
+                      <div>
+                          @if(empty($shop->filename))
+                            <img src="{{ asset('images/no_image.jpg')}}">
+                          @else
+                            <img src="{{ asset('storage/shops/' . $shop->filename)}}">
+                          @endif
+                      </div>
+                    </div>
+                    </a>
+                    </div>
                   @endforeach
               </div>
           </div>
