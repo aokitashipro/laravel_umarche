8a39c64f4dbc66581b4dd9480b2ff0e3f8e1663a
aokitashipro <webmocha2@gmail.com>
2021-05-22 08:39:50 +0900

    image初期設定

---
+++ database/migrations/2021_05_22_081510_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->string('filename');
+            $table->string('title')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
--- routes/owner.php
+++ routes/owner.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,6 +33,10 @@ Route::prefix('shops')->
         Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
         Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 });
+
+Route::resource('images', ImageController::class)
+->middleware('auth:owners')->except(['show']);
+
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
