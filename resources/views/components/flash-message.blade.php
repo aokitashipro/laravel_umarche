ea1951bd4f684015d98ab7c0b5c97fa348ee649d
aokitashipro <webmocha2@gmail.com>
2021-05-23 07:31:02 +0900

    コンポーネント修正

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -6,7 +6,7 @@ if(session('status') === 'alert'){$bgColor = 'bg-red-500';}
 @endphp
 
 @if(session('message'))
-  <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
+  <div class="{{ $bgColor }} w-1/2 mx-auto p-2 my-4 text-white">
     {{ session('message' )}}
   </div>
 @endif
--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -10,7 +10,7 @@
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200">
                 <x-flash-message status="session('status')" /> 
-                <div class="flex justify-end mt-4 mb-4"> 
+                <div class="flex justify-end mb-4"> 
                   <button onclick="location.href='{{ route('owner.images.create')}}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>                        
                 </div> 
                 <div class="flex flex-wrap">
