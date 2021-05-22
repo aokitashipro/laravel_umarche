b48b887c8a211455412030fcf0e8abfdaed2f29b
aokitashipro <webmocha2@gmail.com>
2021-05-23 05:26:42 +0900

    edit, update

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -76,38 +76,27 @@ class ImageController extends Controller
         'status' => 'info']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit($id)
     {
-        //
+        $image = Image::findOrFail($id);
+        return view('owner.images.edit', compact('image'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title' => 'string|max:50'
+        ]);
+
+        $image = Image::findOrFail($id);
+        $image->title = $request->title;
+        $image->save();
+
+        return redirect()
+        ->route('owner.images.index')
+        ->with(['message' => '画像情報を更新しました。',
+        'status' => 'info']);
     }
 
     /**
--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -15,13 +15,11 @@
                 </div> 
                 <div class="flex flex-wrap">
                 @foreach ($images as $image )
-                    <div class="w-1/4 p-4">
+                    <div class="w-1/4 p-2 md:p-4">
                     <a href="{{ route('owner.images.edit', ['image' => $image->id ])}}">  
-                    <div class="border rounded-md p-4">
-                      
-                      <div class="text-xl">{{ $image->title }}</div>
+                    <div class="border rounded-md p-2 md:p-4">
                       <x-thumbnail :filename="$image->filename" type="products" />
-
+                      <div class="text-gray-700">{{ $image->title }}</div>
                     </div>
                     </a>
                     </div>
