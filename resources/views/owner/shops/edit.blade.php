f8379aee96496bb9bc71e14f2a59e04ec43cbfad
aokitashipro <webmocha2@gmail.com>
2021-05-22 14:04:23 +0900

    radio @if周り

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -39,9 +39,9 @@
                         </div>
                       </div>
                       <div class="p-2 w-1/2 mx-auto">
-                        <div class="relative">
-                          <input type="radio" name="is_selling" value="1" @if($shop->is_selling === 1){ checked } @endif >販売中
-                          <input type="radio" name="is_selling" value="0" @if($shop->is_selling === 0){ checked } @endif >停止中
+                        <div class="relative flex justify-around">
+                          <div><input type="radio" name="is_selling" value="1" class="mr-2" @if($shop->is_selling === 1){ checked } @endif >販売中</div>
+                          <div><input type="radio" name="is_selling" value="0" class="mr-2" @if($shop->is_selling === 0){ checked } @endif >停止中</div>
                         </div>
                       </div>
                           <div class="p-2 w-full flex justify-around mt-4">
