ad380619acaa1145efe584b65e48d46ce0418335
aokitashipro <webmocha2@gmail.com>
2021-05-27 05:27:07 +0900

    product index, eager loading

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -4,17 +4,49 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('product'); 
+            if(!is_null($id)){ 
+            $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId; 
+                if($productId !== Auth::id()){ 
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
+
     public function index()
     {
-        //
+        //$products = Owner::findOrFail(Auth::id())->shop->product;
+        
+        $ownerInfo = Owner::with('shop.product.imageFirst')
+        ->where('id', Auth::id())->get();
+
+        // dd($ownerInfo);
+        // foreach($ownerInfo as $owner){
+        // //    dd($owner->shop->product);
+        //     foreach($owner->shop->product as $product){
+        //         dd($product->imageFirst->filename);
+        //     }
+        // }
+
+        return view('owner.products.index',
+        compact('ownerInfo'));
     }
 
     /**
--- resources/views/layouts/owner-navigation.blade.php
+++ resources/views/layouts/owner-navigation.blade.php
@@ -23,6 +23,9 @@
                     <x-nav-link :href="route('owner.images.index')" :active="request()->routeIs('owner.images.index')">
                         画像管理
                     </x-nav-link>
+                    <x-nav-link :href="route('owner.products.index')" :active="request()->routeIs('owner.products.index')">
+                        商品管理
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -79,6 +82,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('owner.images.index')" :active="request()->routeIs('owner.images.index')">
                 画像管理
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('owner.products.index')" :active="request()->routeIs('owner.products.index')">
+                商品管理
             </x-responsive-nav-link>
         </div>
 
