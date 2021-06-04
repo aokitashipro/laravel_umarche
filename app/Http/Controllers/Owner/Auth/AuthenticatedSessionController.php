8662da130d3c5cf8572beb11d7adb04927a899e3
aokitashipro <webmocha2@gmail.com>
2021-06-05 05:57:32 +0900

    ログアウト時のリダイレクト修正

--- app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
@@ -49,6 +49,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/owner');
+        return redirect('/owner/login');
     }
 }
