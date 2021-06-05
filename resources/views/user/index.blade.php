dc4a30f9c4bc267eb741ec3b1e3ecb7568626ff2
aokitashipro <webmocha2@gmail.com>
2021-06-06 05:18:35 +0900

    view調整

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -16,7 +16,11 @@
                        <a href="">  
                        <div class="border rounded-md p-2 md:p-4">
                          <x-thumbnail filename="{{$product->imageFirst->filename ?? ''}}" type="products" />
-                         <div class="text-gray-700">{{ $product->name }}</div>
+                            <div class="mt-4">
+                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category->name }}</h3>
+                                <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
+                                <p class="mt-1">{{ number_format($product->price) }}<span class="text-sm text-gray-700">円(税込)</span></p>
+                              </div>
                        </div>
                        </a>
                        </div>
