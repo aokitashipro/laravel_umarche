e23f02c31847e479fdd51e905a86eb95c825f101
aokitashipro <webmocha2@gmail.com>
2021-06-05 13:15:40 +0900

    dashboardコメントアウト

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::middleware('auth:users')->group(function(){
         Route::get('/', [ItemController::class, 'index'])->name('items.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('user.dashboard');
-})->middleware(['auth:users'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('user.dashboard');
+// })->middleware(['auth:users'])->name('dashboard');
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
