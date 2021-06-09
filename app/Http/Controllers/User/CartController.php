67703f3affab93a484f95a36ab1919a14a0dd458
aokitashipro <webmocha2@gmail.com>
2021-06-10 06:38:10 +0900

    cart delete

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -45,4 +45,13 @@ class CartController extends Controller
         
         return redirect()->route('user.cart.index');
     }
+
+    public function delete($id)
+    {
+        Cart::where('product_id', $id)
+        ->where('user_id', Auth::id())
+        ->delete();
+
+        return redirect()->route('user.cart.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::middleware('auth:users')->group(function(){
 Route::prefix('cart')->middleware('auth:users')->group(function(){
         Route::get('/', [CartController::class, 'index'])->name('cart.index');
         Route::post('add', [CartController::class, 'add'])->name('cart.add');   
+        Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
 });
 
 // Route::get('/dashboard', function () {
