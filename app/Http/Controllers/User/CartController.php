38b96575cba2aa1a76513897a2a9b192eb10b189
aokitashipro <webmocha2@gmail.com>
2021-06-11 06:05:52 +0900

    stripe controller

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -54,4 +54,40 @@ class CartController extends Controller
 
         return redirect()->route('user.cart.index');
     }
+
+    public function checkout()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        
+        $lineItems = [];
+        foreach($products as $product){
+            $lineItem = [
+                'name' => $product->name,
+                'description' => $product->information,
+                'amount' => $product->price,
+                'currency' => 'jpy',
+                'quantity' => $product->pivot->quantity,
+            ];
+            array_push($lineItems, $lineItem);
+        }
+        // dd($lineItems);
+
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [$lineItems],
+            'mode' => 'payment',
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index'),
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return view('user.checkout', 
+            compact('session', 'publicKey'));
+
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::prefix('cart')->middleware('auth:users')->group(function(){
         Route::get('/', [CartController::class, 'index'])->name('cart.index');
         Route::post('add', [CartController::class, 'add'])->name('cart.add');   
         Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
+        Route::get('checkout', [CartController::class, 'checkout'])->name('cart.checkout');
 });
 
 // Route::get('/dashboard', function () {
