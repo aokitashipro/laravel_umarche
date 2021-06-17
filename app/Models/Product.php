152a7f8044bc04d822a7bbe9241a370d97f7f300
aokitashipro <webmocha2@gmail.com>
2021-06-18 04:12:41 +0900

    local scope

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,7 @@ use App\Models\SecondaryCategory;
 use App\Models\Image;
 use App\Models\Stock;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
@@ -67,6 +68,29 @@ class Product extends Model
     {
         return $this->belongsToMany(User::class, 'carts')
         ->withPivot(['id', 'quantity']);
+    }
+
+    public function scopeAvailableItems($query)
+    {
+        $stocks = DB::table('t_stocks')
+        ->select('product_id', 
+        DB::raw('sum(quantity) as quantity'))
+        ->groupBy('product_id')
+        ->having('quantity', '>', 1);
+
+        return $query
+        ->joinSub($stocks, 'stock', function($join){
+            $join->on('products.id', '=', 'stock.product_id');
+        })
+        ->join('shops', 'products.shop_id', '=', 'shops.id')
+        ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
+        ->join('images as image1', 'products.image1', '=', 'image1.id')
+        ->where('shops.is_selling', true)
+        ->where('products.is_selling', true)
+        ->select('products.id as id', 'products.name as name', 'products.price'
+        ,'products.sort_order as sort_order'
+        ,'products.information', 'secondary_categories.name as category'
+        ,'image1.filename as filename');
     }
     
 
