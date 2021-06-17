348eecaaf0d571f2c2b09606b0ad83e1409065db
aokitashipro <webmocha2@gmail.com>
2021-06-18 05:18:07 +0900

    コンストラクタの修正

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -13,6 +13,18 @@ class ItemController extends Controller
     public function __construct()
     {
         $this->middleware('auth:users');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('item'); 
+            if(!is_null($id)){ 
+            $itemId = Product::availableItems()->where('products.id', $id)->exists();
+                if(!$itemId){ 
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
     }
         
     public function index()
