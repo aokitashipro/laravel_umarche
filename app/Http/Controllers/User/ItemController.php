e395c8875b8dc5f40f27025adecbaf04918d0735
aokitashipro <webmocha2@gmail.com>
2021-06-20 05:43:49 +0900

    pagination初期値設定

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -31,7 +31,7 @@ class ItemController extends Controller
     {
         $products = Product::availableItems()
         ->sortOrder($request->sort)
-        ->paginate($request->pagination);
+        ->paginate($request->pagination ?? '20');
 
         return view('user.index', compact('products'));
     }
