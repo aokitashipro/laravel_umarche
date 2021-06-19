fab1189afe80f6554e1e216ecad80da762969b69
aokitashipro <webmocha2@gmail.com>
2021-06-20 05:09:11 +0900

    表示件数

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -31,7 +31,7 @@ class ItemController extends Controller
     {
         $products = Product::availableItems()
         ->sortOrder($request->sort)
-        ->get();
+        ->paginate($request->pagination);
 
         return view('user.index', compact('products'));
     }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -37,7 +37,26 @@
                                 </option>
                             </select>
                         </div>
-                        <div>表示件数</div>
+                        <div>
+                            <span class="text-sm">表示件数</span><br>
+                            <select id="pagination" name="pagination">
+                                <option value="20"
+                                    @if(\Request::get('pagination') === '20')
+                                    selected
+                                    @endif>20件
+                                </option>
+                                <option value="50"
+                                    @if(\Request::get('pagination') === '50')
+                                    selected
+                                    @endif>50件
+                                </option>
+                                <option value="100"
+                                    @if(\Request::get('pagination') === '100')
+                                    selected
+                                    @endif>100件
+                                </option>
+                            </select>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -66,6 +85,10 @@
                        @endforeach
                      
                      </div>
+                     {{ $products->appends([
+                         'sort' => \Request::get('sort'),
+                         'pagination' => \Request::get('pagination')
+                     ])->links() }}
                 </div>
             </div>
         </div>
@@ -73,6 +96,10 @@
 <script>
     const select = document.getElementById('sort')
     select.addEventListener('change', function(){
+        this.form.submit()
+    })
+    const paginate = document.getElementById('pagination')
+    paginate.addEventListener('change', function(){
         this.form.submit()
     })
 
