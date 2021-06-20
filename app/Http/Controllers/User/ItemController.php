ea07458146c5761bd5b042d8d725b970dc444c61
aokitashipro <webmocha2@gmail.com>
2021-06-21 06:35:27 +0900

    検索フォーム キーワード

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -37,6 +37,7 @@ class ItemController extends Controller
 
         $products = Product::availableItems()
         ->selectCategory($request->category ?? '0')
+        ->searchKeyword($request->keyword)
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? '20');
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -123,6 +123,29 @@ class Product extends Model
         }
     }
 
+    public function scopeSearchKeyword($query, $keyword)
+    {
+        if(!is_null($keyword))
+        {
+           //全角スペースを半角に
+           $spaceConvert = mb_convert_kana($keyword,'s');
+
+           //空白で区切る
+           $keywords = preg_split('/[\s]+/', $spaceConvert,-1,PREG_SPLIT_NO_EMPTY);
+
+           //単語をループで回す
+           foreach($keywords as $word)
+           {
+               $query->where('products.name','like','%'.$word.'%');
+           }
+
+           return $query;  
+
+        } else {
+            return;
+        }
+    }
+
 
 
 }
