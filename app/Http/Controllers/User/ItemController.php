6443d52dc7c782287128b0e840c03b1644935da4
aokitashipro <webmocha2@gmail.com>
2021-06-23 05:05:54 +0900

    キュー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,7 @@ use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -33,9 +34,14 @@ class ItemController extends Controller
     public function index(Request $request)
     {
         // dd($request);
-        Mail::to('test@example.com')
-        ->send(new TestMail());
+
+        // 同期的に送信
+        // Mail::to('test@example.com')
+        // ->send(new TestMail());
         
+        // 非同期に送信
+        SendThanksMail::dispatch();
+
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
