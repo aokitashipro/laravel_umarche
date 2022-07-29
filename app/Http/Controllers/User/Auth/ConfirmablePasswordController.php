3b07d1b1a78c5cba97692828341b27e714b66a5f
aokimonpro <43558838+aokitashipro@users.noreply.github.com>
2022-07-29 10:47:57 +0900

    Update ConfirmablePasswordController.php
    Auth::guard('user') -> Auth::guard('users') に変更しました。

--- app/Http/Controllers/User/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/User/Auth/ConfirmablePasswordController.php
@@ -29,7 +29,7 @@ class ConfirmablePasswordController extends Controller
      */
     public function store(Request $request)
     {
-        if (! Auth::guard('user')->validate([
+        if (! Auth::guard('users')->validate([
             'email' => $request->user()->email,
             'password' => $request->password,
         ])) {
